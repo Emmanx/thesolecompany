d0cd3973971dfea725e4cb6c54ae7ae617ffcd97
Chizo <nwazuochizo@gmail.com>
2020-09-08 00:13:41 +0100

    for deploy

--- js/index.js
+++ js/index.js
@@ -9,130 +9,7 @@ var animation = bodymovin.loadAnimation({
 
 gsap.registerPlugin(ScrollTrigger);
 
-// let moveIn = gsap
-// 	.to('.js-t-line', {
-// 		scrollTrigger: 'js-t-line',
-// 		duration: 1,
-// 		y: 0,
-// 		ease: 'ease-out',
-// 	})
-// 	.pause();
-
-//remake nodes with animation classes
-function slideUpAnimationWrapper(selector) {
-	let upForAnim = document.querySelectorAll(selector);
-	upForAnim.forEach((el) => {
-		let content = el.textContent;
-		let fragment = new DocumentFragment();
-		var oh = document.createElement('span');
-		oh.classList.add('oh');
-		var jsT = document.createElement('span');
-		jsT.classList.add('js-t-line');
-		jsT.textContent = content;
-		oh.appendChild(jsT);
-		fragment.appendChild(oh);
-		el.textContent = '';
-		el.appendChild(fragment);
-	});
-}
-
-slideUpAnimationWrapper('.slide-up-reveal');
-
-function slideUpAnimation() {
-	gsap.utils.toArray('.js-t-line').forEach((el) => {
-		gsap.to(el, {
-			scrollTrigger: {
-				trigger: el,
-				once: false,
-			},
-			delay: 0.3,
-			duration: 1,
-			ease: 'expo',
-			y: 0,
-			onComplete: () => console.log('Im done'),
-		});
-	});
-}
-
-function lineAnimation() {
-	gsap.utils.toArray('.divider').forEach((el) => {
-		gsap.to(el, {
-			scrollTrigger: {
-				trigger: el,
-			},
-			delay: 0.3,
-			duration: 1.5,
-			ease: 'expo',
-			width: '100%',
-		});
-	});
-}
-
-function otherAnimations() {
-	gsap.to('.js-t-line-head', {
-		duration: 1,
-		ease: 'expo',
-		y: 0,
-		stagger: 0.2,
-	});
-
-	gsap.to('.js-t-line-stg', {
-		duration: 1,
-		ease: 'expo',
-		y: 0,
-		delay: 0.5,
-		stagger: 0.3,
-	});
-}
-
-window.onload = () => {
-	setTimeout(() => {
-		pageTransitionIn().then(() => {
-			document.querySelector('body').classList.remove('is-loading');
-		});
-		pageTransitionOut().then(() => {
-			// sequence page animations
-			slideUpAnimation();
-			lineAnimation();
-			otherAnimations();
-		});
-	}, 1000);
-};
-
-function pageTransitionIn() {
-	return gsap.to('.page-transition', {
-		duration: 0.5,
-		scaleX: 1,
-		transformOrigin: 'bottom right',
-	});
-}
-
-function pageTransitionOut() {
-	return gsap.timeline({ delay: 1 }).to('.page-transition', {
-		duration: 0.5,
-		scaleX: 0,
-		skewX: 0,
-		transformOrigin: 'top left',
-		ease: 'power1.out',
-	});
-}
-
-barba.init({
-	transitions: [
-		{
-			name: 'say-hi',
-			leave() {
-				pageTransitionOut();
-			},
-			enter() {
-				pageTransitionIn();
-				let slide = new TextSlide(document.querySelector('.slide'));
-				slide.play();
-			},
-		},
-	],
-});
-
+//text changer animation
 class Slide {
 	constructor(el) {
 		this.el = el;
@@ -140,6 +17,7 @@ class Slide {
 	}
 }
 
+let slidePlay;
 class TextSlide {
 	constructor(slideNode) {
 		this.slideNode = slideNode;
@@ -179,13 +57,151 @@ class TextSlide {
 	}
 
 	play() {
-		this.isPlay = setInterval(() => this.switch(), 2500);
+		this.switch();
+		slidePlay = setInterval(() => this.switch(), 2500);
+		// return this.isPlay;
 	}
 
 	stop() {
-		clearInterval(this.isPlay);
+		if (!slidePlay) return;
+		clearInterval(slidePlay);
 	}
 }
 
-let slide = new TextSlide(document.querySelector('.slide'));
-slide.play();
+//remake nodes with animation classes
+function slideUpAnimationWrapper(selector) {
+	let upForAnim = document.querySelectorAll(selector);
+	upForAnim.forEach((el) => {
+		let content = el.textContent;
+		let fragment = new DocumentFragment();
+		var oh = document.createElement('span');
+		oh.classList.add('oh');
+		var jsT = document.createElement('span');
+		jsT.classList.add('js-t-line');
+		jsT.textContent = content;
+		oh.appendChild(jsT);
+		fragment.appendChild(oh);
+		el.textContent = '';
+		el.appendChild(fragment);
+	});
+}
+
+slideUpAnimationWrapper('.slide-up-reveal');
+
+function slideUpAnimation() {
+	gsap.utils.toArray('.js-t-line').forEach((el) => {
+		gsap.to(el, {
+			scrollTrigger: {
+				trigger: el,
+				start: 'top+=20 bottom',
+			},
+			delay: 0.3,
+			duration: 1.5,
+			ease: 'expo',
+			y: 0,
+			onComplete: () => console.log('Im done'),
+		});
+	});
+}
+
+function lineAnimation() {
+	gsap.utils.toArray('.divider').forEach((el) => {
+		gsap.to(el, {
+			scrollTrigger: {
+				trigger: el,
+			},
+			delay: 0.3,
+			duration: 1.5,
+			ease: 'expo',
+			width: '100%',
+		});
+	});
+}
+
+function slideInLeftAnimation() {
+	gsap.utils.toArray('.slide-in-left').forEach((el) => {
+		gsap.to(el, {
+			scrollTrigger: {
+				trigger: el,
+				start: 'top+=40 bottom',
+			},
+			delay: 0.6,
+			duration: 1,
+			ease: 'ease-in',
+			x: 0,
+			opacity: 1,
+		});
+	});
+}
+
+function otherAnimations() {
+	gsap.to('.js-t-line-head', {
+		duration: 1,
+		ease: 'expo',
+		y: 0,
+		stagger: 0.2,
+	});
+
+	gsap.to('.js-t-line-stg', {
+		duration: 1,
+		ease: 'expo',
+		y: 0,
+		delay: 0.5,
+		stagger: 0.3,
+	});
+}
+
+window.onload = () => {
+	setTimeout(() => {
+		pageTransitionIn().then(() => {
+			document.querySelector('body').classList.remove('is-loading');
+		});
+		pageTransitionOut();
+	}, 1000);
+};
+
+function pageTransitionIn() {
+	return gsap.to('.page-transition', {
+		duration: 0.5,
+		scaleX: 1,
+		transformOrigin: 'bottom right',
+	});
+}
+
+function pageTransitionOut() {
+	return gsap
+		.timeline({ delay: 1 })
+		.to('.page-transition', {
+			duration: 0.5,
+			scaleX: 0,
+			skewX: 0,
+			transformOrigin: 'top left',
+			ease: 'power1.out',
+		})
+		.then(() => {
+			let slide = new TextSlide(document.querySelector('.slide'));
+			slide.stop();
+			slide.play();
+			// sequence page animations
+			slideUpAnimation();
+			lineAnimation();
+			slideInLeftAnimation();
+			otherAnimations();
+		});
+}
+
+barba.init({
+	transitions: [
+		{
+			name: 'say-hi',
+			leave() {
+				pageTransitionOut();
+			},
+			enter() {
+				pageTransitionIn();
+				let slide = new TextSlide(document.querySelector('.slide'));
+				slide.play();
+			},
+		},
+	],
+});
